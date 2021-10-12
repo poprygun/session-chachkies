35b7fa4767098a12ca7fb16caddf43cc949828ba
Alex Shumilov <poprygun@hotmail.com>
2021-10-12 12:53:23 -0400

    Add session logging.

---
+++ src/main/java/io/microsamples/sessions/sessionchachkies/config/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package io.microsamples.sessions.sessionchachkies.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Bean
+    public SessionInterceptor getSessionInterceptor() {
+        return new SessionInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getSessionInterceptor()).addPathPatterns("/*");
+    }
+}
---
+++ src/main/java/io/microsamples/sessions/sessionchachkies/config/SessionInterceptor.java
@@ -0,0 +1,20 @@
+package io.microsamples.sessions.sessionchachkies.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Slf4j
+public class SessionInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse arg1,
+                           Object arg2, ModelAndView arg3) {
+        log.info("👀 {}", request.getSession().getId());
+    }
+
+}
